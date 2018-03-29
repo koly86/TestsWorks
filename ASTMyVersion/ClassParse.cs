42173eaedc21042eec72acd3541e7b68035f6062
Nikolay Moriakov <koly86@gmail.com>
2018-03-29 13:37:22 +0300

    ю

--- ASTMyVersion/ClassParse.cs
+++ ASTMyVersion/ClassParse.cs
@@ -8,6 +8,7 @@ namespace MyVersion
         private readonly char[] _numbers = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
         private readonly char[] _operations = {'+', '-', '/', '*', '(', ')'};
         public char[] ArrOfExpression { get; set; }
+        private int c; //считаем количество пустых полей в массиве
 
         public ClassParse(string expression)
         {
@@ -15,25 +16,32 @@ namespace MyVersion
             var arrOfExpression = new char[expression.Length];
 
             for (var i = 0; i < expression.Length; i++)
-                if (!expression[i].Equals(null))
+                if (!expression[i].Equals(null) && !expression[i].Equals(0) )
                 {
                     foreach (var t in _operations)
                         if (expression[i] == t)
                         {
                             arrOfExpression[i] = t;
-                           // Console.WriteLine($@"operation {t}");
                         }
 
                     foreach (var t1 in _numbers)
                         if (expression[i] == t1)
                         {
                             arrOfExpression[i] = t1;
-                           // Console.WriteLine($@"number  {t1}");
                         }
                 }
 
             Console.WriteLine();
-            foreach (var ex in arrOfExpression) Console.Write(ex);
+            for (var i = 0; i < arrOfExpression.Length; i++)
+              if (arrOfExpression[i] == 0)
+                  c++;
+
+            foreach (var ex in arrOfExpression)
+            {
+                Console.Write(ex);
+            }
+
+           // 
             Console.WriteLine();
             ArrOfExpression = arrOfExpression;
         }
